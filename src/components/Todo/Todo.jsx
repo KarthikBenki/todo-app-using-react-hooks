d2ab902478c9e7490ef74ae81c86f4f67efcdc0c
Karthik M C <karthik.mc@thoughtworks.com>
2022-12-12 17:48:33 +0530

    create simple todo app to add and delete todo list

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,11 +1,69 @@
-import React from "react";
-import "./Todo.css"
+import React, { useState } from "react";
+import "./Todo.css";
+import todoImg from "../../images/todo-image.jpeg";
 
 const Todo = () => {
+  const [inputData, setInputData] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (e) => {
+    setInputData(e.target.value);
+  };
+
+  const addTodo = () => {
+    if (!inputData) return;
+    setTodoList([...todoList, inputData]);
+    setInputData("");
+  };
+
+  const removeAll = () => {
+    setTodoList([]);
+  };
+
+  const deleteItem = (ind) => {
+    const updatedList = todoList.filter((element, id) => {
+      return id !== ind;
+    });
+
+    setTodoList(updatedList);
+  };
+
   return (
     <>
       <div className="main-container">
-        <h1>Hello World</h1>
+        <div className="todo-container">
+          <div className="todo-image">
+            <img src={todoImg} alt="" />
+          </div>
+          <div className="todo-input">
+            <input
+              type="text"
+              placeholder="Enter Todos..."
+              value={inputData}
+              onChange={handleChange}
+            />
+            <button className="add-todo-button" onClick={addTodo}>
+              Add Todo
+            </button>
+          </div>
+
+          <div className="todo-display">
+            {todoList.map((element, ind) => {
+              return (
+                <div className="todo-display-list" key={ind}>
+                  <h3>
+                    {element}{" "}
+                    <button onClick={() => deleteItem(ind)}>Delete</button>
+                  </h3>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="remove-all">
+            <button onClick={removeAll}>Remove All</button>
+          </div>
+        </div>
       </div>
     </>
   );
