811a8aa049176323e710e1db6ac10bace90cc825
Karthik M C <karthik.mc@thoughtworks.com>
2022-12-12 16:49:41 +0530

    create todo component and render hello world in the center

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Todo from './components/Todo/Todo';
 
 function App() {
   return (
     <>
-    <h1>Hello World</h1>
+    <Todo/>
     </>
   );
 }
---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Todo.css"
+
+const Todo = () => {
+  return (
+    <>
+      <div className="main-container">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
